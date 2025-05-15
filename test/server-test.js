67b815319ae939c271bedac898d09530058ea39b
martinsalinas0 <martinlsalinas@pm.me>
2025-05-14 23:04:08 -0500

    added test for get api/products

--- test/server-test.js
+++ test/server-test.js
@@ -1,2 +1,19 @@
 const mocha = require('mocha')
 const chai = require('chai'); 
+const expect = require('chai').expect; 
+
+
+describe('GET all products api', () => { 
+  it('should return all products in database', (done) => { 
+    chai
+    .request(server)
+    .get('/api/products')
+    .end((err, res) => { 
+      res.should.have.status(200); 
+      res.should.be.an('object'); 
+      res.should.have.proprty('products'); 
+      res.body.products.should.be.an('opbject'); 
+      done()
+    })
+  })
+})
